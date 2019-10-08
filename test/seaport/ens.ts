ec56cd78551eb1a5f9f8e1f51454ea899204b7f0
Alexander Atallah <alexander.atallah@gmail.com>
2019-10-08 11:48:28 +0900

    refactor tests to allow skipping some

--- test/seaport/ens.ts
+++ test/seaport/ens.ts
@@ -10,10 +10,10 @@ import {
 } from 'mocha-typescript'
 
 import { OpenSeaPort, orderToJSON } from '../../src/index'
-import * as Web3 from 'web3'
 import { Network, Order, WyvernSchemaName, OpenSeaAsset } from '../../src/types'
 import { RINKEBY_API_KEY } from '../constants'
 import { MnemonicWalletSubprovider } from '@0x/subproviders'
+import * as _ from 'lodash'
 import * as RPCSubprovider from 'web3-provider-engine/subproviders/rpc'
 import * as Web3ProviderEngine from 'web3-provider-engine'
 
@@ -37,13 +37,13 @@ const rinkebyClient = new OpenSeaPort(providerEngine, {
 }, line => console.info(`RINKEBY: ${line}`))
 
 const ENS_ADDRESS = "0x53ceb15b76023fbec5bb39450214926f6aa77d2e"
-const NAMES = [ "123.eth", "1234.eth", "12345.eth", "123456.eth" ] // "111st.eth"
+const NAMES = [ "123.eth" ]
+// , "1234.eth", "12345.eth", "123456.eth" ]
 
 let wethAddress: string
 let manaAddress: string
 let accountAddress: string
-const tokenIdsForNames: string[] = []
-const auctions: Order[] = []
+let auctions: Order[] = []
 let startAmount: number
 
 suite.only('seaport: ENS names', () => {
@@ -53,39 +53,47 @@ suite.only('seaport: ENS names', () => {
       console.warn("Skipping ENS suite due to no mnemonic or address set.")
       this.skip()
     }
+    const now =  new Date().getTime() / 1000
     accountAddress = ADDRESS
     wethAddress = (await rinkebyClient.api.getPaymentTokens({ symbol: 'WETH'})).tokens[0].address
     manaAddress = (await rinkebyClient.api.getPaymentTokens({ symbol: 'MANA'})).tokens[0].address
+
+    // Iterate through NAMES to find auctions
     for (const name of NAMES) {
       const res = await rinkebyClient.api.get(`/api/v1/misc/ens_short_name_asset/${name}/`)
       const data = await res.json()
-      tokenIdsForNames.push(data.data.asset.token_id)
-    }
-  })
-
-  test("Short names should all exist and have some open auctions", async () => {
-    const now =  new Date().getTime() / 1000
-    for (const tokenId of tokenIdsForNames) {
+      const tokenId = data.data.asset.token_id
       const asset = await rinkebyClient.api.getAsset(ENS_ADDRESS, tokenId)
       if (!asset.sellOrders || !asset.sellOrders.length) {
         continue
       }
+
       const sellOrder = asset.sellOrders[0]
       assert.isAbove(+sellOrder.listingTime, now)
       sellOrder.asset = asset
       auctions.push(sellOrder)
     }
+
+    // Make sure auctions exist
     assert.isNotEmpty(auctions)
+    if (!auctions.length) {
+      return
+    }
+
+    auctions = _.orderBy(auctions, a => a.currentPrice && +a.currentPrice)
+
     const auction = auctions[0]
-    if (!auction || !auction.currentPrice) {
+    assert.isNotNull(auction.currentPrice)
+    if (!auction.currentPrice) {
       return
     }
     startAmount = +auction.currentPrice.dividedBy(1e18)
-    console.warn(`Current time is ${now}`)
     console.warn(`Bid amount is ${startAmount}`)
+    console.warn(`Current time is ${now}`)
+    console.warn(`Payment token is ${wethAddress}`)
   })
 
-  test("Auctions should accept new bids via the SDK", async () => {
+  test.skip("Auctions should accept new bids via the SDK", async () => {
     const auction = auctions[0]
     const asset = auction.asset as OpenSeaAsset
     await rinkebyClient.createBuyOrder({
