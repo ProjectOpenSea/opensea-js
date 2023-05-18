3b6ec0682960f197d6824eff5e81e544eae15021
JoshuaSchmidt-OpenSea <joshua.schmidt@opensea.io>
2023-05-18 14:04:52 -0500

    Move shared initialization logic for integration test to common file

--- src/__integration_tests__/getCollection.ts
+++ src/__integration_tests__/getCollection.ts
@@ -1,35 +1,7 @@
 import { assert } from "chai";
-import { ethers } from "ethers";
 import { suite, test } from "mocha";
-import Web3 from "web3";
-import { Network } from "wyvern-js/lib/types";
-import {
-  ALCHEMY_API_KEY,
-  MAINNET_API_KEY,
-  WALLET_PRIV_KEY,
-} from "../__tests__/constants";
-import { OpenSeaSDK } from "../sdk";
+import { sdk } from "./init";
 import { SafelistStatus } from "../types";
-
-const webProvider = new Web3.providers.HttpProvider(
-  `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`
-);
-
-const rpcProvider = new ethers.providers.JsonRpcProvider(
-  `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`
-);
-
-const wallet = new ethers.Wallet(WALLET_PRIV_KEY ?? "", rpcProvider);
-
-const sdk = new OpenSeaSDK(
-  webProvider,
-  {
-    networkName: Network.Main,
-    apiKey: MAINNET_API_KEY,
-  },
-  (line) => console.info(`MAINNET: ${line}`),
-  wallet
-);
 
 suite("SDK: getCollection", () => {
   test("Get Verified Collection", async () => {
