48a205efb34317d03c804d66dd57e8ef7cb14040
JoshuaSchmidt-OpenSea <joshua.schmidt@opensea.io>
2023-05-19 14:20:00 -0500

    Add collection stats to integration test

--- src/types.ts
+++ src/types.ts
@@ -323,69 +323,89 @@ interface StringTraitStats {
 /**
  * Annotated collection stats with OpenSea
  */
-export interface OpenSeaCollectionStats{
-    one_minute_volume: number;
-    one_minute_change: number;
-    one_minute_sales: number;
-    one_minute_sales_change: number;
-    one_minute_average_price: number;
-    one_minute_difference: number;
-    five_minute_volume: number;
-    five_minute_change: number;
-    five_minute_sales: number;
-    five_minute_sales_change: number;
-    five_minute_average_price: number;
-    five_minute_difference: number;
-    fifteen_minute_volume: number;
-    fifteen_minute_change: number;
-    fifteen_minute_sales: number;
-    fifteen_minute_sales_change: number;
-    fifteen_minute_average_price: number;
-    fifteen_minute_difference: number;
-    thirty_minute_volume: number;
-    thirty_minute_change: number;
-    thirty_minute_sales: number;
-    thirty_minute_sales_change: number;
-    thirty_minute_average_price: number;
-    thirty_minute_difference: number;
-    one_hour_volume: number;
-    one_hour_change: number;
-    one_hour_sales: number;
-    one_hour_sales_change: number;
-    one_hour_average_price: number;
-    one_hour_difference: number;
-    six_hour_volume: number;
-    six_hour_change: number;
-    six_hour_sales: number;
-    six_hour_sales_change: number;
-    six_hour_average_price: number;
-    six_hour_difference: number;
-    one_day_volume: number;
-    one_day_change: number;
-    one_day_sales: number;
-    one_day_sales_change: number;
-    one_day_average_price: number;
-    one_day_difference: number;
-    seven_day_volume: number;
-    seven_day_change: number;
-    seven_day_sales: number;
-    seven_day_average_price: number;
-    seven_day_difference: number;
-    thirty_day_volume: number;
-    thirty_day_change: number;
-    thirty_day_sales: number;
-    thirty_day_average_price: number;
-    thirty_day_difference: number;
-    total_volume: number;
-    total_sales: number;
-    total_supply: number;
-    count: number;
-    num_owners: number;
-    average_price: number;
-    num_reports: number;
-    market_cap: number;
-    floor_price: number;
-  }
+export interface OpenSeaCollectionStats {
+  // One Minute
+  one_minute_volume: number;
+  one_minute_change: number;
+  one_minute_difference: number;
+  one_minute_sales: number;
+  one_minute_sales_change: number;
+  one_minute_average_price: number;
+
+  // Five Minute
+  five_minute_volume: number;
+  five_minute_change: number;
+  five_minute_difference: number;
+  five_minute_sales: number;
+  five_minute_sales_change: number;
+  five_minute_average_price: number;
+
+  // Fifteen Minute
+  fifteen_minute_volume: number;
+  fifteen_minute_change: number;
+  fifteen_minute_difference: number;
+  fifteen_minute_sales: number;
+  fifteen_minute_sales_change: number;
+  fifteen_minute_average_price: number;
+
+  // Thirty Minute
+  thirty_minute_volume: number;
+  thirty_minute_change: number;
+  thirty_minute_difference: number;
+  thirty_minute_sales: number;
+  thirty_minute_sales_change: number;
+  thirty_minute_average_price: number;
+
+  // One Hour
+  one_hour_volume: number;
+  one_hour_change: number;
+  one_hour_sales: number;
+  one_hour_sales_change: number;
+  one_hour_average_price: number;
+  one_hour_difference: number;
+
+  // Six Hour
+  six_hour_volume: number;
+  six_hour_change: number;
+  six_hour_sales: number;
+  six_hour_sales_change: number;
+  six_hour_average_price: number;
+  six_hour_difference: number;
+
+  // One Day
+  one_day_volume: number;
+  one_day_change: number;
+  one_day_sales: number;
+  one_day_sales_change: number;
+  one_day_average_price: number;
+  one_day_difference: number;
+
+  // Seven Day
+  seven_day_volume: number;
+  seven_day_change: number;
+  seven_day_sales: number;
+  seven_day_average_price: number;
+  seven_day_difference: number;
+
+  // Thirty Day
+  thirty_day_volume: number;
+  thirty_day_change: number;
+  thirty_day_sales: number;
+  thirty_day_average_price: number;
+  thirty_day_difference: number;
+
+  // Total
+  total_volume: number;
+  total_sales: number;
+  total_supply: number;
+  count: number;
+  num_owners: number;
+  average_price: number;
+  num_reports: number;
+  market_cap: number;
+  floor_price: number;
+}
+
 /**
  * Annotated collection with OpenSea metadata
  */
