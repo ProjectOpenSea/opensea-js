2c33198945d1811ca20a7fc005a247f7d2fec1e8
Ryan Ghods <ryan@ryanio.com>
2024-03-25 12:12:28 -0700

    Revert "update default offer amount"
    This reverts commit 620366c3e0b0542aa48746cfafca2f5672fd43c1.

--- test/utils/constants.ts
+++ test/utils/constants.ts
@@ -15,7 +15,7 @@ export const RPC_PROVIDER_POLYGON = new ethers.JsonRpcProvider(
   `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY_POLYGON}`,
 );
 
-export const OFFER_AMOUNT = process.env.OFFER_AMOUNT ?? "0.00001";
+export const OFFER_AMOUNT = process.env.OFFER_AMOUNT ?? "0.004";
 export const DAPPER_ADDRESS = "0x4819352bd7fadcCFAA8A2cDA4b2825a9ec51417c";
 export const BAYC_CONTRACT_ADDRESS =
   "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d";
